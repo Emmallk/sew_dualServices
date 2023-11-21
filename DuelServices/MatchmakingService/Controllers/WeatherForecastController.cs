32dcd0e58fd2ed0ce8e98ae7639980b23b8471ca
emmakarner <e.karner@htlkrems.at>
2023-11-21 09:01:53 +0100

    project

---
+++ DuelServices/MatchmakingService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchmakingService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly HttpClient _httpClient;
+    
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        await _httpClient.GetAsync("https://google.com");
+        var forecast = await _httpClient.GetFromJsonAsync<List<WeatherForecast>>("http:/google.com"); //Lesen GetRequest
+       
+        _httpClient.PostAsJsonAsync("https://localhost:0000/WeatherForecast", forecast); //Schreiben Post ..
+        
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+} 
